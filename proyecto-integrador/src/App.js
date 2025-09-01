18afbc07079919c63ef0593fd1a4819aa0ddcd34
ldenti@udesa.edu.ar <ldenti@udesa.edu.ar>
2025-09-01 12:13:49 -0300

    estructura de carpetas terminada

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./screens/Home/Home";
+import Personajes from "./screens/Personajes/Personajes";
+import AboutUs from "./screens/AboutUs/AboutUs";
+import Error from "./screens/Error/Error";
+import UnPersonaje from "./screens/UnPersonaje/UnPersonaje";
+import { Route, Switch } from "react-router-dom/cjs/react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+
+    </Switch>
   );
 }
 
