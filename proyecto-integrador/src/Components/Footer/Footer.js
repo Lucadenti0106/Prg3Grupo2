330da556f85acb7252feb984117e349a49474b69
ldenti@udesa.edu.ar <ldenti@udesa.edu.ar>
2025-09-18 14:17:31 -0300

    cambiando cosas del footer

--- proyecto-integrador/src/Components/Footer/Footer.js
+++ proyecto-integrador/src/Components/Footer/Footer.js
@@ -7,6 +7,7 @@ function Footer() {
         <h4>RodeCo Movies</h4>
         <p>RodeCo es por <strong>Ro</strong>lon <strong>De</strong>nti y <strong>Co</strong>lombo por si no estaba claro</p>
         <p> Luca Denti, Benito Colombo, Agustin Rolon De Bary</p>
+        <img src="images.gif" alt="Cargando..." />
     </footer>
 );
 }
